9f7c989192cd4b6ba9135423adcc4f4194520626
chensq <chensq@dreamdt.cn>
2022-05-10 18:11:18 +0800

    add:广告轮播

---
+++ src/components/IBanner.vue
@@ -0,0 +1,580 @@
+<template>
+  <!--
+    根目录规范(必须不能为空)：
+    idm-ctrl：控件类型 drag_container：容器，drag_container_inlieblock：行内容器，idm_module：非容器的组件
+    id：使用moduleObject.id，如果id不使用这个将会被idm-ctrl-id属性替换
+    idm-ctrl-id：组件的id，这个必须不能为空
+  -->
+  <div
+    idm-ctrl="idm_module"
+    :id="moduleObject.id"
+    :idm-ctrl-id="moduleObject.id"
+    :title="propData.htmlTitle"
+    v-show="propData.defaultStatus != 'hidden'"
+    @click="textClickHandle"
+    class="box"
+  >
+    <div class="box-swiper">
+      <div class="box-swiper-container">
+        <ul class="swiper-wrapper">
+          <li
+            class="swiper-slide box-swiper-item-container"
+            v-for="(item, index) in propData.list"
+            :key="index"
+            @click="handleClickSwiperItem(item)"
+          >
+            <img :src="item.img" class="slider-img" alt="" />
+            <span class="box-swiper-text">{{item.title}}</span>
+          </li>
+        </ul>
+        <div class="swiper-pagination"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Swiper from "swiper";
+import "swiper/dist/css/swiper.min.css";
+export default {
+  name: "IBanner",
+  data() {
+    return {
+      moduleObject: {},
+      currentSwiperIndex: 0,
+      propData: this.$root.propData.compositeAttr || {
+        htmlTitle: "广告轮播",
+        width: "100%",
+        height: "auto",
+        list: [
+          {
+            img: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fp7.itc.cn%2Fimages01%2F20200903%2F9f80293e09644046a408f8be7359d4ff.jpeg&refer=http%3A%2F%2Fp7.itc.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654761049&t=b2726a52f403b5d4dcca968820d55109",
+            title:
+              "标题标题标题，这是标题，这是他标题，标题标题标题，这是标题，这是他标题，标题标题标题，这是标题，这是他标题，标题标题标题，这是标题，这是他标题，标题标题标题，这是标题，这是他标题，",
+          },
+          {
+            img: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fp6.itc.cn%2Fimages01%2F20201012%2F19ebfe9ad37b4e29bac785eb0146d02e.jpeg&refer=http%3A%2F%2Fp6.itc.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654761049&t=5100670fe8877ffd3ad76e0f0f7bb6a0",
+            title:
+              "标题标题标题，这是标题，这是他标题，标题标题标题，这是标题，这是他标题，标题标题标题，这是标题，这是他标题，标题标题标题，这是标题，这是他标题，标题标题标题，这是标题，这是他标题，",
+          },
+          {
+            img: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fp1.itc.cn%2Fq_70%2Fimages03%2F20210129%2Fcb5a1f799b364202beed7f122e87bf8a.png&refer=http%3A%2F%2Fp1.itc.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654761049&t=8a20f26c2574d17e04376728ba796324",
+            title:
+              "标题标题标题，这是标题，这是他标题，标题标题标题，这是标题，这是他标题，标题标题标题，这是标题，这是他标题，标题标题标题，这是标题，这是他标题，标题标题标题，这是标题，这是他标题，",
+          },
+          {
+            img: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fp6.itc.cn%2Fimages01%2F20201012%2F19ebfe9ad37b4e29bac785eb0146d02e.jpeg&refer=http%3A%2F%2Fp6.itc.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654761049&t=5100670fe8877ffd3ad76e0f0f7bb6a0",
+            title:
+              "标题标题标题，这是标题，这是他标题，标题标题标题，这是标题，这是他标题，标题标题标题，这是标题，这是他标题，标题标题标题，这是标题，这是他标题，标题标题标题，这是标题，这是他标题，",
+          },
+        ],
+      },
+    };
+  },
+  props: {},
+  created() {
+    this.moduleObject = this.$root.moduleObject;
+    // console.log(this.moduleObject)
+    this.convertAttrToStyleObject();
+  },
+  mounted() {
+    this.initSwiper();
+  },
+  destroyed() {},
+  methods: {
+    handleClickSwiperItem(item){
+      console.log(item)
+    },
+    initSwiper() {
+      var mySwiper = new Swiper(".box-swiper-container", {
+        autoplay:2000,
+        speed:500,
+        loop:true,
+        loopedSlides:5,
+        slidesPerView:'auto',
+        effect:'coverflow',
+        pagination:{
+          el: '.swiper-pagination',
+          bulletClass : 'my-bullet',
+          bulletActiveClass: 'my-bullet-active',
+        },
+        centeredSlides:true,
+        coverflowEffect:{
+          rotate:0,
+          stretch: -25,
+          depth:100,
+          modifier:1,
+          slideShadows: false,
+        },
+        onInit: function (swiper) {
+          //回调函数，初始化后执行。
+          swiper.slides[2].className = "swiper-slide swiper-slide-active"; //第一次打开不要动画
+        },
+      });
+    },
+    handleClickItem(item) {
+      console.log(item);
+    },
+    handleClickMore() {},
+    /**
+     * 提供父级组件调用的刷新prop数据组件
+     */
+    propDataWatchHandle(propData) {
+      this.propData = propData.compositeAttr || {};
+      this.convertAttrToStyleObject();
+    },
+    /**
+     * 把属性转换成样式对象
+     */
+    convertAttrToStyleObject() {
+      var styleObject = {};
+      if (this.propData.bgSize && this.propData.bgSize == "custom") {
+        styleObject["background-size"] =
+          (this.propData.bgSizeWidth
+            ? this.propData.bgSizeWidth.inputVal +
+              this.propData.bgSizeWidth.selectVal
+            : "auto") +
+          " " +
+          (this.propData.bgSizeHeight
+            ? this.propData.bgSizeHeight.inputVal +
+              this.propData.bgSizeHeight.selectVal
+            : "auto");
+      } else if (this.propData.bgSize) {
+        styleObject["background-size"] = this.propData.bgSize;
+      }
+      if (this.propData.positionX && this.propData.positionX.inputVal) {
+        styleObject["background-position-x"] =
+          this.propData.positionX.inputVal + this.propData.positionX.selectVal;
+      }
+      if (this.propData.positionY && this.propData.positionY.inputVal) {
+        styleObject["background-position-y"] =
+          this.propData.positionY.inputVal + this.propData.positionY.selectVal;
+      }
+      for (const key in this.propData) {
+        if (this.propData.hasOwnProperty.call(this.propData, key)) {
+          const element = this.propData[key];
+          if (!element && element !== false && element != 0) {
+            continue;
+          }
+          switch (key) {
+            case "width":
+            case "height":
+              styleObject[key] = element;
+              break;
+            case "box":
+              if (element.marginTopVal) {
+                styleObject["margin-top"] = `${element.marginTopVal}`;
+              }
+              if (element.marginRightVal) {
+                styleObject["margin-right"] = `${element.marginRightVal}`;
+              }
+              if (element.marginBottomVal) {
+                styleObject["margin-bottom"] = `${element.marginBottomVal}`;
+              }
+              if (element.marginLeftVal) {
+                styleObject["margin-left"] = `${element.marginLeftVal}`;
+              }
+              if (element.paddingTopVal) {
+                styleObject["padding-top"] = `${element.paddingTopVal}`;
+              }
+              if (element.paddingRightVal) {
+                styleObject["padding-right"] = `${element.paddingRightVal}`;
+              }
+              if (element.paddingBottomVal) {
+                styleObject["padding-bottom"] = `${element.paddingBottomVal}`;
+              }
+              if (element.paddingLeftVal) {
+                styleObject["padding-left"] = `${element.paddingLeftVal}`;
+              }
+              break;
+            case "bgImgUrl":
+              styleObject[
+                "background-image"
+              ] = `url(${window.IDM.url.getWebPath(element)})`;
+              break;
+            case "positionX":
+              //背景横向偏移
+
+              break;
+            case "positionY":
+              //背景纵向偏移
+
+              break;
+            case "bgRepeat":
+              //平铺模式
+              styleObject["background-repeat"] = element;
+              break;
+            case "bgAttachment":
+              //背景模式
+              styleObject["background-attachment"] = element;
+              break;
+            case "border":
+              if (element.border.top.width > 0) {
+                styleObject["border-top-width"] =
+                  element.border.top.width + element.border.top.widthUnit;
+                styleObject["border-top-style"] = element.border.top.style;
+                if (element.border.top.colors.hex8) {
+                  styleObject["border-top-color"] =
+                    element.border.top.colors.hex8;
+                }
+              }
+              if (element.border.right.width > 0) {
+                styleObject["border-right-width"] =
+                  element.border.right.width + element.border.right.widthUnit;
+                styleObject["border-right-style"] = element.border.right.style;
+                if (element.border.right.colors.hex8) {
+                  styleObject["border-right-color"] =
+                    element.border.right.colors.hex8;
+                }
+              }
+              if (element.border.bottom.width > 0) {
+                styleObject["border-bottom-width"] =
+                  element.border.bottom.width + element.border.bottom.widthUnit;
+                styleObject["border-bottom-style"] =
+                  element.border.bottom.style;
+                if (element.border.bottom.colors.hex8) {
+                  styleObject["border-bottom-color"] =
+                    element.border.bottom.colors.hex8;
+                }
+              }
+              if (element.border.left.width > 0) {
+                styleObject["border-left-width"] =
+                  element.border.left.width + element.border.left.widthUnit;
+                styleObject["border-left-style"] = element.border.left.style;
+                if (element.border.left.colors.hex8) {
+                  styleObject["border-left-color"] =
+                    element.border.left.colors.hex8;
+                }
+              }
+
+              styleObject["border-top-left-radius"] =
+                element.radius.leftTop.radius +
+                element.radius.leftTop.radiusUnit;
+              styleObject["border-top-right-radius"] =
+                element.radius.rightTop.radius +
+                element.radius.rightTop.radiusUnit;
+              styleObject["border-bottom-left-radius"] =
+                element.radius.leftBottom.radius +
+                element.radius.leftBottom.radiusUnit;
+              styleObject["border-bottom-right-radius"] =
+                element.radius.rightBottom.radius +
+                element.radius.rightBottom.radiusUnit;
+              break;
+            case "font":
+              styleObject["font-family"] = element.fontFamily;
+              if (element.fontColors.hex8) {
+                styleObject["color"] = element.fontColors.hex8;
+              }
+              styleObject["font-weight"] =
+                element.fontWeight && element.fontWeight.split(" ")[0];
+              styleObject["font-style"] = element.fontStyle;
+              styleObject["font-size"] =
+                element.fontSize + element.fontSizeUnit;
+              styleObject["line-height"] =
+                element.fontLineHeight +
+                (element.fontLineHeightUnit == "-"
+                  ? ""
+                  : element.fontLineHeightUnit);
+              styleObject["text-align"] = element.fontTextAlign;
+              styleObject["text-decoration"] = element.fontDecoration;
+              break;
+          }
+        }
+      }
+      window.IDM.setStyleToPageHead(this.moduleObject.id, styleObject);
+      this.initData();
+    },
+    /**
+     * 通用的url参数对象
+     * 所有地址的url参数转换
+     */
+    commonParam() {
+      let urlObject = IDM.url.queryObject();
+      var params = {
+        pageId:
+          window.IDM.broadcast && window.IDM.broadcast.pageModule
+            ? window.IDM.broadcast.pageModule.id
+            : "",
+        urlData: JSON.stringify(urlObject),
+      };
+      return params;
+    },
+    /**
+     * 重新加载
+     */
+    reload() {
+      //请求数据源
+      this.initData();
+    },
+    /**
+     * 加载动态数据
+     */
+    initData() {
+      let that = this;
+      //所有地址的url参数转换
+      var params = that.commonParam();
+      switch (this.propData.dataSourceType) {
+        case "customInterface":
+          this.propData.customInterfaceUrl &&
+            window.IDM.http
+              .get(this.propData.customInterfaceUrl, params)
+              .then((res) => {
+                //res.data
+                that.$set(
+                  that.propData,
+                  "list",
+                  that.getExpressData(
+                    "resultData",
+                    that.propData.dataFiled,
+                    res.data
+                  )
+                );
+                // that.propData.fontContent = ;
+              })
+              .catch(function (error) {});
+          break;
+        case "pageCommonInterface":
+          //使用通用接口直接跳过，在setContextValue执行
+          break;
+        case "customFunction":
+          if (
+            this.propData.customFunction &&
+            this.propData.customFunction.length > 0
+          ) {
+            var resValue = "";
+            try {
+              resValue =
+                window[this.propData.customFunction[0].name] &&
+                window[this.propData.customFunction[0].name].call(this, {
+                  ...params,
+                  ...this.propData.customFunction[0].param,
+                  moduleObject: this.moduleObject,
+                });
+            } catch (error) {}
+            that.propData.list = resValue;
+          }
+          break;
+      }
+    },
+    /**
+     * 通用的获取表达式匹配后的结果
+     */
+    getExpressData(dataName, dataFiled, resultData) {
+      //给defaultValue设置dataFiled的值
+      var _defaultVal = undefined;
+      if (dataFiled) {
+        var filedExp = dataFiled;
+        filedExp = dataName + (filedExp.startsWiths("[") ? "" : ".") + filedExp;
+        var dataObject = { IDM: window.IDM };
+        dataObject[dataName] = resultData;
+        _defaultVal = window.IDM.express.replace.call(
+          this,
+          "@[" + filedExp + "]",
+          dataObject
+        );
+      }
+      //对结果进行再次函数自定义
+      if (
+        this.propData.customFunction &&
+        this.propData.customFunction.length > 0
+      ) {
+        var params = this.commonParam();
+        var resValue = "";
+        try {
+          resValue =
+            window[this.propData.customFunction[0].name] &&
+            window[this.propData.customFunction[0].name].call(this, {
+              ...params,
+              ...this.propData.customFunction[0].param,
+              moduleObject: this.moduleObject,
+              expressData: _defaultVal,
+              interfaceData: resultData,
+            });
+        } catch (error) {}
+        _defaultVal = resValue;
+      }
+
+      return _defaultVal;
+    },
+    /**
+     * 文本点击事件
+     */
+    textClickHandle() {
+      let that = this;
+      if (this.moduleObject.env == "develop") {
+        //开发模式下不执行此事件
+        return;
+      }
+      //获取所有的URL参数、页面ID（pageId）、以及所有组件的返回值（用范围值去调用IDM提供的方法取出所有的组件值）
+      let urlObject = window.IDM.url.queryObject(),
+        pageId =
+          window.IDM.broadcast && window.IDM.broadcast.pageModule
+            ? window.IDM.broadcast.pageModule.id
+            : "";
+      //自定义函数
+      /**
+       * [
+       * {name:"",param:{}}
+       * ]
+       */
+      var clickFunction = this.propData.clickFunction;
+      clickFunction &&
+        clickFunction.forEach((item) => {
+          window[item.name] &&
+            window[item.name].call(this, {
+              urlData: urlObject,
+              pageId,
+              customParam: item.param,
+              _this: this,
+            });
+        });
+    },
+    showThisModuleHandle() {
+      this.propData.defaultStatus = "default";
+    },
+    hideThisModuleHandle() {
+      this.propData.defaultStatus = "hidden";
+    },
+    /**
+     * 组件通信：接收消息的方法
+     * @param {
+     *  type:"发送消息的时候定义的类型，这里可以自己用来要具体做什么，统一规定的type：linkageResult（组件联动传结果值）、linkageDemand（组件联动传需求值）、linkageReload（联动组件重新加载）
+     * 、linkageOpenDialog（打开弹窗）、linkageCloseDialog（关闭弹窗）、linkageShowModule（显示组件）、linkageHideModule（隐藏组件）、linkageResetDefaultValue（重置默认值）"
+     *  message:{发送的时候传输的消息对象数据}
+     *  messageKey:"消息数据的key值，代表数据类型是什么，常用于表单交互上，比如通过这个key判断是什么数据"
+     *  isAcross:如果为true则代表发送来源是其他页面的组件，默认为false
+     * } object
+     */
+    receiveBroadcastMessage(object) {
+      console.log("组件收到消息", object);
+      if (object.type && object.type == "linkageShowModule") {
+        this.showThisModuleHandle();
+      } else if (object.type && object.type == "linkageHideModule") {
+        this.hideThisModuleHandle();
+      }
+    },
+    /**
+     * 组件通信：发送消息的方法
+     * @param {
+     *  type:"自己定义的，统一规定的type：linkageResult（组件联动传结果值）、linkageDemand（组件联动传需求值）、linkageReload（联动组件重新加载）
+     * 、linkageOpenDialog（打开弹窗）、linkageCloseDialog（关闭弹窗）、linkageShowModule（显示组件）、linkageHideModule（隐藏组件）、linkageResetDefaultValue（重置默认值）",
+     *  message:{实际的消息对象},
+     *  rangeModule:"为空发送给全部，根据配置的属性中设定的值（值的内容是组件的packageid数组），不取子表的，比如直接 this.$root.propData.compositeAttr["attrKey"]（注意attrKey是属性中定义的bindKey）,这里的格式为：['1','2']"",
+     *  className:"指定的组件类型，比如只给待办组件发送，然后再去过滤上面的值"
+     *  globalSend:如果为true则全站发送消息，注意全站rangeModule是无效的，只有className才有效，默认为false
+     * } object
+     */
+    sendBroadcastMessage(object) {
+      window.IDM.broadcast && window.IDM.broadcast.send(object);
+    },
+    /**
+     * 交互功能：设置组件的上下文内容值
+     * @param {
+     *  type:"定义的类型，已知类型：pageCommonInterface（页面统一接口返回值）、"
+     *  key:"数据key标识，页面每个接口设置的数据集名称，方便识别获取自己需要的数据"
+     *  data:"数据集，内容为：字符串 or 数组 or 对象"
+     * }
+     */
+    setContextValue(object) {
+      console.log("统一接口设置的值", object);
+      if (object.type != "pageCommonInterface") {
+        return;
+      }
+      //这里使用的是子表，所以要循环匹配所有子表的属性然后再去设置修改默认值
+      if (object.key == this.propData.dataName) {
+        // this.propData.fontContent = this.getExpressData(this.propData.dataName,this.propData.dataFiled,object.data);
+        this.$set(
+          this.propData,
+          "list",
+          this.getExpressData(
+            this.propData.dataName,
+            this.propData.dataFiled,
+            object.data
+          )
+        );
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.box {
+  &-swiper-container {
+    position: relative;
+    width: 100%;
+    height: 200px;
+  }
+  &-swiper-item-container {
+    width: 90%;
+    height: 200px;
+    margin: 0 auto;
+    position: relative;
+    top: 0;
+    left: 0;
+  }
+  &-swiper-text {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    color: #fff;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    padding: 4px 100px 4px 8px;
+    background-color: rgba(0, 0, 0, 0.3);
+    font-size: 14px;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+  }
+}
+
+
+li {
+  display: inline-block;
+  float: left;
+  height: 200px;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+.swiper-slide {
+  -webkit-transition: transform .5s;
+  -moz-transition: transform .5s;
+  -ms-transition: transform .5s;
+  -o-transition: transform .5s;
+  -webkit-transform: scale(1);
+  transform: scale(0.9);
+  position: relative;
+}
+.swiper-slide-active,
+.swiper-slide-duplicate-active {
+  -webkit-transform: scale(1);
+  transform: scale(1);
+}
+.slider-img{
+  border-radius: 8px;
+  overflow: hidden;
+}
+.swiper-pagination{
+  left: 34% !important;
+  bottom: 5px !important;
+}
+</style>
+<style>
+
+.my-bullet{
+  width: 6px;
+  height: 6px;
+  display: inline-block;
+  border-radius: 0;
+  transform: rotate(45deg);
+  background-color: #fff;
+  margin: 0 3px;
+}
+.my-bullet-active{
+  background-color: #1D335E ;
+}
+</style>
