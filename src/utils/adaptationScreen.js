98100e4c76c2bb856674ac1bdacaccdc66b1d70c
chensq <chensq@dreamdt.cn>
2022-05-26 09:17:25 +0800

    perf:计算适配

--- src/utils/adaptationScreen.js
+++ src/utils/adaptationScreen.js
@@ -9,10 +9,16 @@ export const getComputedSize = function (initSize) {
   const WINDOW_WIDTH = this.pageWidth                                     // 当前屏幕宽度
   const BASE_SIZE = Number(this.propData.baseSize)                        // 配置屏幕基准值宽度
   const ADAPTATION_RATIO = Number(this.propData.adaptationRatio)          // 配置适配比例
-  if(!WINDOW_WIDTH || !BASE_SIZE || !ADAPTATION_RATIO) {
-    console.error(WINDOW_WIDTH, BASE_SIZE, ADAPTATION_RATIO, 'calculate error')
+  const FINAL_RESULT = initSize * ((WINDOW_WIDTH / BASE_SIZE - 1) * (ADAPTATION_RATIO - 1) + 1)
+  if(!WINDOW_WIDTH || !BASE_SIZE || !ADAPTATION_RATIO || window.isNaN(FINAL_RESULT)) {
+    console.error(`
+      WINDOW_WIDTH      ---->  ${WINDOW_WIDTH}
+      BASE_SIZE         ---->  ${BASE_SIZE}
+      ADAPTATION_RATIO  ---->  ${ADAPTATION_RATIO}
+      FINAL_RESULT      ---->  ${FINAL_RESULT}
+      "Calculate's params error"
+    `)
     return initSize
   }
-  const FINAL_RESULT = initSize * ((WINDOW_WIDTH / BASE_SIZE - 1) * (ADAPTATION_RATIO - 1) + 1)
   return Math.round(FINAL_RESULT)
 }
